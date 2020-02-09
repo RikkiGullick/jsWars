1b16d9860ce4c9f3868d4a91ff74dc17c44ee078
Jennywren111 <54098563+Jennywren111@users.noreply.github.com>
2020-02-09 18:49:22 +0000

    Added a placeholder score.

--- Scripts/game/game.js
+++ Scripts/game/game.js
@@ -4,15 +4,18 @@ import StatsBar from './statsBar.js';
 import View from './view.js';
 import EntityManager from '../entities/entityManager.js';
 
+// What's creating the instance of GameSystem? Oh, read to the end Jen.... :D (PS Sorry, I know in real life you can't waffle your way all over the code like I am doing! I will delete all this stuff before submitting PR!)
+
 class GameSystem {
     entityManager = null;;
     _view = null;
     _player = null;
     _tick = 0;
+    _score = ""; // Have set this to string for now cos realised my placeholder is a string, not numeric!! 
 
     constructor() {
         this.entityManager = new EntityManager();
-        this.stats = new StatsBar();
+        this.stats = new StatsBar();  // Aha - found it! So this creates an object called 'stats', created from class 'StatsBar', inside object instance of GameSystem class. This class now has my new method in it which makes a jQuery object from the score div and when the setScore method is run, adds placeholder text to the div.
         this._view = new View();
     }
 
@@ -38,6 +41,7 @@ class GameSystem {
         this._tick++;
 
         this.stats.setTick(this._tick);
+        this.stats.setScore(); // so when method '_run' is called, the object instance 'stats' runs method setScore using the placeholder in the class. Err I think.  (Head explodes from too many things inside things inside things) I intially copied the line above with this._score but then realised the method didn't do anything with that. 
     }
 }
 
--- Scripts/game/statsBar.js
+++ Scripts/game/statsBar.js
@@ -1,13 +1,19 @@
 ﻿
 export default class StatsBar {
     _$tick = null;
+    _$score = null; // null means an object is expected
     
     constructor() {
         this._$tick = $('#tick');
+        this._$score = $('#score'); // grabs div id score and turns it into jQuery object
     }
 
     setTick(value) {
         this._$tick.text(value);
     }
 
+    setScore() {
+        this._$score.text("Placeholder for the current score");
+    }
+    // So we have a new method in the class now. We need to go and find where setScore should be called. Something presumably should create a new instance of StatsBar, when a new game is started? I guess we will need to call it there? Hmm, let's go see. (Skips off in search....)
 };
