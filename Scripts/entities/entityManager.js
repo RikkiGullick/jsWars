1b5432d742f60cff6cbe3c24f683a07932b5af43
Jennywren111 <54098563+Jennywren111@users.noreply.github.com>
2020-03-08 18:37:39 +0000

    Work in progress - moved createElement to entityManager but problem passing $element back to enemy1.

--- Scripts/entities/entityManager.js
+++ Scripts/entities/entityManager.js
@@ -46,7 +46,7 @@ export default class EntityManager {
         // I will need to make a new function like the one below in order for my new enemy to appear. Will set it similarly to start with, then look at timings later. I may switch off enemy1 temporarily whilst I work on enemy2.
 
         if (Math.random() < createFactor) { // createFactor keeps increasing so creation of enemy1 is increased?? Maybe I could switch it to 'greater than' so they are gradually reduced? Or add another clause (what's the word for this? Expression?) to this if statement so they only get created when tick is between two certain figures.
-            new Enemy1(); // Hmm. This is interesting. It's not storing this as a variable - it just 'runs' the constructor??? Probably the constructor function stores it to an array?
+            new Enemy1(); // Hmm. This is interesting. It's not storing this as a variable - it just 'runs' the constructor??? Probably cos the constructor function stores it to an array?
         }
 
         if (tick % 500 === 0 && Math.random() < 0.3) { // each time the tick reaches a multiple of 500, there's a 30% chance a planet will be created.
@@ -65,7 +65,7 @@ export default class EntityManager {
     // experimenting with moving this here from enemy1, but how do I then pass the $element back to enemy1? Can I type the name of the class like I've done below, or do I need the name of the instance? Hmm... The instance is being sent straight to an array. So how do I refer to the instance??? And even if typing Enemy1 works, what if Enemy2 wants to use this function? I should be able to work this out... But I can't. Grr.
     createElement () {
         console.log("Testing whether createElement is running in entityManager")
-        Enemy1.$element = $('<div></div>').addClass('entity enemy1');
+        Enemy1.$element = $('<div><p style="color: #CC99FF">BADDY!</p></div>').addClass('entity enemy1'); // this is the problem line I think. 
     }
 
 };
