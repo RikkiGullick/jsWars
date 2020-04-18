017f0320ed3bbc7ffb20571f9e2b70b62c9fdff1
Jennywren111 <54098563+Jennywren111@users.noreply.github.com>
2020-04-18 08:38:12 +0100

    Tidied entityManager to restore enemy waves, removed comments.

--- Scripts/entities/entityManager.js
+++ Scripts/entities/entityManager.js
@@ -1,8 +1,7 @@
 ﻿
 import CollisionManager from "./collisionManager.js";
 import Enemy1 from "../enemy/enemy1.js";
-import Enemy2 from "../enemy/enemy2.js"; // import my new enemy
-import Enemy from "../enemy/enemy1.js";
+import Enemy2 from "../enemy/enemy2.js"; 
 import Planet from "../background/planet.js";
 
 export default class EntityManager {
@@ -29,36 +28,33 @@ export default class EntityManager {
         return this._entities;
     }
 
-    update (tick) {  // runs when called by 'run' in the loop in game instance. 
+    update (tick) { 
         var _this = this;
         this._entities.forEach(function (entity, index) {
             entity.update(tick);
         });
         this._collisionManager.process(this._entities);
         this._entities = this._entities.filter(function (entity) { return !entity.destroyed; });
-        this._createNewStuff(tick); // with each 'tick' this runs the function below which controls what entities to create and when.
+        this._createNewStuff(tick); 
     }
 
-    _createNewStuff (tick) {    // Looks like I can manipulate this function to add my new enemy at certain points of the tick.
+    _createNewStuff (tick) {    
         var createFactor = 0.04;
         if (tick > 400) createFactor += 0.02;   
-        if (tick > 800) createFactor += 0.02; // Adding to createFactor makes new enemy more likely.  
-
-        // I will need to make a new function like the one below in order for my new enemy to appear. Will set it similarly to start with, then look at timings later. I may switch off enemy1 temporarily whilst I work on enemy2.
-
+        if (tick > 800) createFactor += 0.02;  
         
-        if ( /* Math.random() < createFactor && tick < 1200 */ tick < 10) { // createFactor keeps increasing so creation of enemy1 is increased?? Maybe I could switch it to 'greater than' so they are gradually reduced? Or add another clause (what's the word for this? Expression?) to this if statement so they only get created when tick is between two certain figures.
-            new Enemy1(); // Hmm. This is interesting. It's not storing this as a variable - it just 'runs' the constructor??? Probably cos the constructor function stores it to an array?
+        
+        if ( Math.random() < createFactor && tick < 1200 ) { 
+            new Enemy1();
         } 
 
         
-        if ( /*Math.random() < createFactor && */ tick < 10 ) { 
+        if ( Math.random() < createFactor && (tick > 1200) && (tick < 2400) ) { 
             new Enemy2(); 
         }
-        
-        
 
-        if (tick % 500 === 0 && Math.random() < 0.3) { // each time the tick reaches a multiple of 500, there's a 30% chance a planet will be created.
+
+        if (tick % 500 === 0 && Math.random() < 0.3) {
             new Planet();
         }
     }
@@ -70,12 +66,6 @@ export default class EntityManager {
     getBounds () {
         return this._bounds;
     }
-
-    // experimenting with moving this here from enemy1, but how do I then pass the $element back to enemy1? Can I type the name of the class like I've done below, or do I need the name of the instance? Hmm... The instance is being sent straight to an array. So how do I refer to the instance??? And even if typing Enemy1 works, what if Enemy2 wants to use this function? I should be able to work this out... But I can't. Grr.
-    /* createElement () {
-        console.log("Testing whether createElement is running in entityManager")
-        Enemy1.$element = $('<div><p style="color: #CC99FF">BADDY!</p></div>').addClass('entity enemy1'); // this is the problem line I think. 
-    } */
 
 };
 
